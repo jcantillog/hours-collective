a669a54ce2c9489deef4358bb76c524673ddb50d
Jonathan Cantillo <jcantillog@unicartagena.edu.co>
2019-06-05 16:38:43 -0500

    Refactoring layout header and implementing logout feature

--- src/components/UI/Layout/index.jsx
+++ src/components/UI/Layout/index.jsx
@@ -1,14 +1,49 @@
-import React from "react";
+import React, { Component } from "react";
+import { withRouter } from 'react-router-dom';
+import PropTypes from "prop-types";
+import {
+    AppBar,
+    IconButton,
+    Toolbar,
+    Typography
+} from "@material-ui/core";
+import ExitToApp from '@material-ui/icons/ExitToApp';
+import SpeakerNotes from '@material-ui/icons/SpeakerNotes';
 /* Styles */
 import "./style.css";
 
-const Layout = ({ children, header }) => {
-  return (
-    <div className="layout">
-      <header className="layout-header">{header}</header>
-      <div className="layout-content">{children}</div>
-    </div>
-  );
+class Layout extends Component {
+    render() {
+        const {header, children} = this.props;
+        return <div className="layout">
+            <AppBar position="static" className="layout-header">
+                <Toolbar className="layout-header-toolbar">
+                    <Typography variant="h6" className="layout-header-title">
+                        <SpeakerNotes style={{marginRight: 10}}/> {header}
+                    </Typography>
+                    <IconButton className="layout-header-exit" onClick={() => this.handlerSignOut()}>
+                        <ExitToApp />
+                    </IconButton>
+                </Toolbar>
+            </AppBar>
+            <div className="layout-content">{children && children}</div>
+        </div>
+    };
+
+    handlerSignOut = () => {
+        localStorage.removeItem("element");
+        this.handlerRouteChange();
+    };
+
+    handlerRouteChange() {
+        let path = `/login`;
+        this.props.history.push(path);
+    }
+}
+
+Layout.propTypes = {
+    header: PropTypes.string.isRequired,
+    children: PropTypes.object,
 };
 
-export default Layout;
+export default withRouter(Layout);
--- src/screens/index.jsx
+++ src/screens/index.jsx
@@ -1,14 +1,13 @@
 import React from "react";
 /* Components */
 import Layout from "../components/UI/Layout";
-
 /* Style */
 import "./style.css";
 
 export default ({ location }) => {
   return (
-    <Layout header="Hours collective">
-      Content
-    </Layout>
+      <Layout header="Hours Collective">
+          Content
+      </Layout>
   );
 };
