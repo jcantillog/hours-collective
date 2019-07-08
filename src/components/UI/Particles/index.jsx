b2c0486705f03d584512e3db9f3c525ef282c299
Jonathan Cantillo <jcantillog@unicartagena.edu.co>
2019-07-08 16:38:24 -0500

    Particles JS added

---
+++ src/components/UI/Particles/index.jsx
@@ -0,0 +1,398 @@
+/* Basic */
+import React from "react";
+import Particles from "react-particles-js";
+/* Styles */
+import styled from 'styled-components';
+const Container = styled.div`
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+`;
+
+const paramsConfig = {
+    simple: {
+        "particles": {
+            "number": {
+                "value": 50
+            },
+            "size": {
+                "value": 3
+            }
+        },
+        "interactivity": {
+            "events": {
+                "onhover": {
+                    "enable": true,
+                    "mode": "repulse"
+                }
+            }
+        }
+    },
+    connected: {
+        particles: {
+            number: {
+                value: 80,
+                density: {
+                    enable: true,
+                    value_area: 1000
+                }
+            },
+            color: {
+                value: "#ffffff"
+            },
+            shape: {
+                type: "circle",
+                stroke: {
+                    width: 0,
+                    color: "#000000"
+                },
+                polygon: {
+                    nb_sides: 5
+                }
+            },
+            opacity: {
+                value: 0.5,
+                random: false,
+                anim: {
+                    enable: false,
+                    speed: 1,
+                    opacity_min: 0.1,
+                    sync: false
+                }
+            },
+            size: {
+                value: 3,
+                random: true,
+                anim: {
+                    enable: false,
+                    speed: 40,
+                    size_min: 0.1,
+                    sync: false
+                }
+            },
+            line_linked: {
+                enable: true,
+                distance: 150,
+                color: "#ffffff",
+                opacity: 0.4,
+                width: 1
+            },
+            move: {
+                enable: true,
+                speed: 3,
+                direction: "none",
+                random: false,
+                straight: false,
+                out_mode: "out",
+                bounce: false,
+                attract: {
+                    enable: false,
+                    rotateX: 600,
+                    rotateY: 1200
+                }
+            }
+        },
+        interactivity: {
+            detect_on: "canvas",
+            events: {
+                onhover: {
+                    enable: true,
+                    mode: "grab"
+                },
+                onclick: {
+                    enable: true,
+                    mode: "push"
+                },
+                resize: true
+            },
+            modes: {
+                grab: {
+                    distance: 140,
+                    line_linked: {
+                        opacity: 1
+                    }
+                },
+                bubble: {
+                    distance: 400,
+                    size: 40,
+                    duration: 2,
+                    opacity: 8,
+                    speed: 3
+                },
+                repulse: {
+                    distance: 200,
+                    duration: 0.4
+                },
+                push: {
+                    particles_nb: 4
+                },
+                remove: {
+                    particles_nb: 2
+                }
+            }
+        },
+        retina_detect: true
+    },
+    dispersion: {
+        "particles": {
+            "number": {
+                "value": 80,
+                "density": {
+                    "enable": true,
+                    "value_area": 800
+                }
+            },
+            "color": {
+                "value": "#ffffff"
+            },
+            "shape": {
+                "type": "circle",
+                "stroke": {
+                    "width": 0,
+                    "color": "#000000"
+                },
+                "polygon": {
+                    "nb_sides": 5
+                },
+                "image": {
+                    "src": "img/github.svg",
+                    "width": 100,
+                    "height": 100
+                }
+            },
+            "opacity": {
+                "value": 0.5,
+                "random": false,
+                "anim": {
+                    "enable": false,
+                    "speed": 1,
+                    "opacity_min": 0.1,
+                    "sync": false
+                }
+            },
+            "size": {
+                "value": 3,
+                "random": true,
+                "anim": {
+                    "enable": false,
+                    "speed": 40,
+                    "size_min": 0.1,
+                    "sync": false
+                }
+            },
+            "line_linked": {
+                "enable": true,
+                "distance": 150,
+                "color": "#ffffff",
+                "opacity": 0.4,
+                "width": 1
+            },
+            "move": {
+                "enable": true,
+                "speed": 6,
+                "direction": "none",
+                "random": false,
+                "straight": false,
+                "out_mode": "out",
+                "bounce": false,
+                "attract": {
+                    "enable": false,
+                    "rotateX": 600,
+                    "rotateY": 1200
+                }
+            }
+        },
+        "interactivity": {
+            "detect_on": "canvas",
+            "events": {
+                "onhover": {
+                    "enable": true,
+                    "mode": "repulse"
+                },
+                "onclick": {
+                    "enable": true,
+                    "mode": "push"
+                },
+                "resize": true
+            },
+            "modes": {
+                "grab": {
+                    "distance": 400,
+                    "line_linked": {
+                        "opacity": 1
+                    }
+                },
+                "bubble": {
+                    "distance": 400,
+                    "size": 40,
+                    "duration": 2,
+                    "opacity": 8,
+                    "speed": 3
+                },
+                "repulse": {
+                    "distance": 200,
+                    "duration": 0.4
+                },
+                "push": {
+                    "particles_nb": 4
+                },
+                "remove": {
+                    "particles_nb": 2
+                }
+            }
+        },
+        "retina_detect": true
+    },
+    bubbles: {
+        "particles": {
+            "number": {
+                "value": 160,
+                "density": {
+                    "enable": false
+                }
+            },
+            "size": {
+                "value": 10,
+                "random": true
+            },
+            "move": {
+                "direction": "bottom",
+                "out_mode": "out"
+            },
+            "line_linked": {
+                "enable": false
+            }
+        },
+        "interactivity": {
+            "events": {
+                "onclick": {
+                    "enable": true,
+                    "mode": "remove"
+                }
+            },
+            "modes": {
+                "remove": {
+                    "particles_nb": 10
+                }
+            }
+        }
+    },
+    "night sky": {
+        "particles": {
+            "number": {
+                "value": 60,
+                "density": {
+                    "enable": true,
+                    "value_area": 1500
+                }
+            },
+            "line_linked": {
+                "enable": true,
+                "opacity": 0.02
+            },
+            "move": {
+                "direction": "right",
+                "speed": 0.05
+            },
+            "size": {
+                "value": 1
+            },
+            "opacity": {
+                "anim": {
+                    "enable": true,
+                    "speed": 1,
+                    "opacity_min": 0.05
+                }
+            }
+        },
+        "interactivity": {
+            "events": {
+                "onclick": {
+                    "enable": true,
+                    "mode": "push"
+                }
+            },
+            "modes": {
+                "push": {
+                    "particles_nb": 1
+                }
+            }
+        },
+        "retina_detect": true
+    },
+    "polygon mask": {
+        "fps_limit": 28,
+        "particles": {
+            "number": {
+                "value": 200,
+                "density": {
+                    "enable": false
+                }
+            },
+            "line_linked": {
+                "enable": true,
+                "distance": 30,
+                "opacity": 0.4
+            },
+            "move": {
+                "speed": 1
+            },
+            "opacity": {
+                "anim": {
+                    "enable": true,
+                    "opacity_min": 0.05,
+                    "speed": 2,
+                    "sync": false
+                },
+                "value": 0.4
+            }
+        },
+        "polygon": {
+            "enable": true,
+            "scale": 0.5,
+            "type": "inline",
+            "move": {
+                "radius": 10
+            },
+            "url": "https://elemento43.com/wp-content/themes/e43/img/logo-e43-color.svg",
+            "inline": {
+                "arrangement": "equidistant"
+            },
+            "draw": {
+                "enable": true,
+                "stroke": {
+                    "color": "rgba(255, 255, 255, .2)"
+                }
+            }
+        },
+        "retina_detect": false,
+        "interactivity": {
+            "events": {
+                "onhover": {
+                    "enable": true,
+                    "mode": "bubble"
+                }
+            },
+            "modes": {
+                "bubble": {
+                    "size": 6,
+                    "distance": 40
+                }
+            }
+        }
+    }
+};
+
+const ParticlesContainer = () => {
+    return (
+        <Container>
+            <Particles
+                params={paramsConfig["connected"]}
+            />
+        </Container>
+    );
+};
+
+export default ParticlesContainer;
--- src/routes.jsx
+++ src/routes.jsx
@@ -1,6 +1,8 @@
+/* Basic */
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import { PrivateRoute } from "./services/utils";
+import ParticlesContainer from "./components/UI/Particles";
 /* Screens */
 import ScreenHome from "./screens";
 import ScreenLogin from "./screens/Login";
@@ -8,6 +10,7 @@ import ScreenLogin from "./screens/Login";
 const Routes = () => {
   return (
     <Router>
+        <ParticlesContainer/>
         <PrivateRoute exact path="/" component={ScreenHome} redirectTo="/login"/>
         <PrivateRoute exact path="/login" component={ScreenLogin} redirectTo="/"/>
     </Router>
